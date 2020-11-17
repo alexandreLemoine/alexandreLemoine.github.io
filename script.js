92e06b82853e77a7ca527431d76de20a2fab2943
Lemoine <lemoine.alexandre.b@gmail.com>
2020-11-17 19:00:46 +0100

    background + cube + menu

---
+++ script.js
@@ -0,0 +1,37 @@
+setTimeout(function(){
+
+    document.getElementById('loadingScreen').remove();
+
+}, 8000);
+
+//*********************** */
+// Changement de section
+
+let cube = document.querySelector('.cube');
+let radioGroup = document.querySelectorAll('.btn-section');
+let currentClass = '';
+
+radioGroup.forEach(function(item){
+
+    item.addEventListener( 'click', function(e){
+
+            //Modification de la section lecture
+            let currentSection = document.querySelector('.section-display');
+            currentSection.classList.remove('section-display');
+
+            console.log(this.value);
+
+            let newSection = document.querySelector('#section-' +  this.value);
+            newSection.classList.add('section-display');
+
+
+            // Modification du cube
+            let showClass = 'show-' + this.value;
+            if ( currentClass ) {
+                cube.classList.remove( currentClass );
+            }
+            cube.classList.add( showClass );
+            currentClass = showClass;
+
+        });
+});
